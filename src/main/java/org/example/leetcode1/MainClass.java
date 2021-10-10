1e77bc89570e35f174cb5e751658b84e835aeeee
huangyh <mrhuangyuhui@gmail.com>
2021-10-10 16:14:49 +0800

    leetcode1

--- src/main/java/org/example/leetcode1/MainClass.java
+++ src/main/java/org/example/leetcode1/MainClass.java
@@ -3,16 +3,17 @@ package org.example.leetcode1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
             }
+            hashtable.put(nums[i], i);
         }
         return new int[0];
     }
