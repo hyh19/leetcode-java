3e1bef588d12013333e9e934df9af4608328eb88
huangyh <mrhuangyuhui@gmail.com>
2021-10-13 13:30:28 +0800

    leetcode20

---
+++ src/main/java/org/example/leetcode20/MainClass.java
@@ -0,0 +1,65 @@
+package org.example.leetcode20;
+
+import com.alibaba.fastjson.JSON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
+public class MainClass {
+    public static String stringToString(String input) {
+        if (input == null) {
+            return "null";
+        }
+        return JSON.parse(input).toString();
+    }
+
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String s = stringToString(line);
+
+            boolean ret = new Solution().isValid(s);
+
+            String out = booleanToString(ret);
+
+            System.out.println(out);
+        }
+    }
+}
